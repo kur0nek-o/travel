705177f2ba9e02a85a756de72985a5c32aed3780
kur0nek-o <maulana21mh@gmail.com>
2023-06-04 10:55:48 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
